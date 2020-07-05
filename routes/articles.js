5b716ca1afcd768bed0506668a91577fd61910f1
Adel <abdelrhman66166@gmail.com>
2020-07-06 00:31:13 +0200

    the end of the tutorial

--- routes/articles.js
+++ routes/articles.js
@@ -8,22 +8,10 @@ router.get('/new', (req, res) => {
 })
 
 // Create Article route
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try {
-        article = await article.save()
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        res.render('articles/new', {
-            article: article,
-            errorMessage: "Error Creating an Article!"
-        })
-    }
-})
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new'))
 
 // show Article route
 router.get('/:slug', async (req, res) => {
@@ -32,10 +20,41 @@ router.get('/:slug', async (req, res) => {
     res.render('articles/show', { article: article })
 })
 
+// edit Article route
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', { article: article })
+})
+
+// update Article route
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
+
 // Delete Article route
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+        try {
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            res.render(`articles/${path}`, {
+                article: article,
+                errorMessage: "Error Creating an Article!"
+            })
+        }
+    }
+}
 
 module.exports = router
