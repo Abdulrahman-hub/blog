9a2122a43a127468f41a43f0a056552615f98a50
Adel <abdelrhman66166@gmail.com>
2020-07-05 19:17:49 +0200

    added the layout and created the veiws folder

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const methodOverride = require('method-override')
 
-app.use('/', (req, res) => {
-    res.send('hello')
+app.set('view engine', 'ejs')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
 app.listen(process.env.PORT || 3000)
