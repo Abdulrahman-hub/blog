50c0670631392ce86030cbd7784115162ee82bb0
Adel <abdelrhman66166@gmail.com>
2020-07-10 21:52:40 +0200

    added a database test

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true,
     useCreateIndex: true
 })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
 
 
 const articleRouter = require('./routes/articles')
